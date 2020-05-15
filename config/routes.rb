7531aed2567b442c0428c7831b1694079727534c
Yasuhiro Usutani <y_usutani@mac.com>
2020-05-15 10:47:31 +0900

    Add messages and comments to routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,9 @@ Rails.application.routes.draw do
   resource :session, only: %i[new create destroy]
   resources :examples, only: :index
 
+  resources :messages, only: %i[index show] do
+    resources :comments, only: :create
+  end
+
   root 'examples#index'
 end
