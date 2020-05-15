d09bd8cc812a5c8ad271a5deeb62253eb7466716
Yasuhiro Usutani <y_usutani@mac.com>
2020-05-15 10:39:06 +0900

    Add destroy to session routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resource :session, only: %i[new create]
+  resource :session, only: %i[new create destroy]
   resources :examples, only: :index
 
   root 'examples#index'
