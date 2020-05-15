8d1d190316c9891eacf23ee19de0c886fcf559f4
Yasuhiro Usutani <y_usutani@mac.com>
2020-05-15 10:18:06 +0900

    Add User seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+big   = User.create! name: 'The Notorious B.I.G.'
+snoop = User.create! name: 'Snoop Dogg'
+flex  = User.create! name: 'Funkmaster Flex'
+ice   = User.create! name: 'Ice Cube'
