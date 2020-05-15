148c54eaf54ebd2e03d43534c70ca3623d57cc3a
Yasuhiro Usutani <y_usutani@mac.com>
2020-05-15 11:52:57 +0900

    Add broadcast to Comment#after_commit

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,15 @@
 class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :message
+
+  after_commit do
+    comment = CommentsController.render(
+      partial: 'comments/comment',
+      locals: { comment: self }
+    )
+    ActionCable.server.broadcast(
+      "messages:#{message_id}:comments",
+      comment: comment
+    )
+  end
 end
