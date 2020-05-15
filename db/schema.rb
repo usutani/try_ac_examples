437f13eaa0a708a2df1398e8ab5a258b787f2aaa
Yasuhiro Usutani <y_usutani@mac.com>
2020-05-15 10:19:16 +0900

    Generate Message model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_15_011616) do
+ActiveRecord::Schema.define(version: 2020_05_15_011841) do
+
+  create_table "messages", force: :cascade do |t|
+    t.string "title"
+    t.text "content"
+    t.integer "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_messages_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
@@ -18,4 +27,5 @@ ActiveRecord::Schema.define(version: 2020_05_15_011616) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "messages", "users"
 end
