ca9a6354951cc492762c24d714bc4bab6e48e7ea
Yasuhiro Usutani <y_usutani@mac.com>
2020-05-15 11:11:29 +0900

    Add stream_from to CommentsChannel#follow

--- app/channels/comments_channel.rb
+++ app/channels/comments_channel.rb
@@ -1,9 +1,10 @@
 class CommentsChannel < ApplicationCable::Channel
-  def subscribed
-    # stream_from "some_channel"
+  def follow(data)
+    stop_all_streams
+    stream_from "messages:#{data['message_id'].to_i}:comments"
   end
 
-  def unsubscribed
-    # Any cleanup needed when channel is unsubscribed
+  def unfollow
+    stop_all_streams
   end
 end
