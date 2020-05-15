fd61697aea71a6988223b84a428a39ac5c9585f5
Yasuhiro Usutani <y_usutani@mac.com>
2020-05-15 10:32:03 +0900

    Call authenticate_user in sessions#create

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,4 +2,9 @@ class SessionsController < ApplicationController
   def new
     @users = User.all
   end
+
+  def create
+    authenticate_user(params[:user_id])
+    redirect_to examples_url
+  end
 end
